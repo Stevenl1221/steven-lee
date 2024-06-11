0c6b80f6ed53f2c7b26a38f9e601c14427729e20
slee51 <slee51@stevens.edu>
2024-06-10 23:58:16 -0400

    delay home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,20 +13,23 @@ export default function Home() {
         "<span class='letter'>$&</span>"
       );
 
-      anime.timeline({ loop: false }).add({
-        targets: ".ml10 .letter",
-        rotateY: [-90, 0],
-        duration: 1300,
-        delay: (el, i) => 45 * i,
-      });
+      anime.timeline({ loop: false }).add(
+        {
+          targets: ".ml10 .letter",
+          rotateY: [-90, 0],
+          duration: 1300,
+          delay: (el, i) => 45 * i,
+        },
+        850
+      );
     }
   }, []);
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <div className="ml10 mb-4 flex align-baseline whitespace-nowrap">
-        <p className="">I&#39;m Steven Lee, a&nbsp;</p>
+        <p className="">I&#39;m Steven Lee</p>
         <span className="text-wrapper">
-          <span className="letters font-bold">Full Stack Engineer.</span>
+          <span className="letters font-bold">, a Full Stack Engineer.</span>
         </span>
       </div>
       <div className="my-8"></div>
