ad5f9aa06977e95305805e7fa73b200c85f2b067
slee51 <slee51@stevens.edu>
2024-06-12 00:05:53 -0400

    fix animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,49 @@
 "use client";
-import Anime, { anime } from "react-anime";
+import { useEffect, useRef } from "react";
+import anime from "animejs";
+
 type LetterProps = {
   children: string;
 };
 
-export default function Home() {
-  const Letter: React.FC<LetterProps> = ({ children }) => {
-    return <span className="letter">{children === " " ? " " : children}</span>;
-  };
+const Letter: React.FC<LetterProps> = ({ children }) => {
+  return <span className="letter">{children === " " ? " " : children}</span>;
+};
+
+const Home: React.FC = () => {
+  const ref = useRef<HTMLSpanElement>(null);
 
   const createSpannedString = (text: string): React.ReactElement[] => {
     return text
       .split("")
-      .map((char) => <Letter key={char}>{char === " " ? " " : char}</Letter>);
+      .map((char, index) => <Letter key={index}>{char}</Letter>);
   };
+
+  useEffect(() => {
+    if (ref.current) {
+      anime({
+        targets: ref.current.children,
+        opacity: [0, 1],
+        // rotateY: [-90, 0],
+        delay: (el, i) => 450 + 45 * i,
+        easing: "easeInOutQuad",
+      });
+    }
+  }, []);
+
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <div className="ml10 mb-4 flex align-baseline whitespace-nowrap lg:text-3xl">
-        <p className="">I&#39;m Steven Lee</p>
+        <p>I&#39;m Steven Lee</p>
         <span className="text-wrapper">
-          <span className="letters font-bold flex flex-row">
-            <Anime
-              rotateY={[-90, 0]}
-              delay={(el: any, i: any) => 1000 + 45 * i}
-            >
-              {createSpannedString(", a Software Engineer.")}
-            </Anime>
+          <span className="letters font-bold flex flex-row" ref={ref}>
+            {createSpannedString(", a Software Engineer.")}
           </span>
         </span>
       </div>
       <div className="my-8"></div>
     </main>
   );
-}
+};
+
+export default Home;
