576d754bf6b7eed2553ab397b6afcaa6b1bc8360
slee51 <slee51@stevens.edu>
2024-06-18 21:13:28 -0400

    update resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex justify-center">
       <embed
-        src="https://drive.google.com/file/d/1Meay7n8BXXFr9hDE3jhNFzVvDr5Yg6oq/preview"
+        src="https://drive.google.com/file/d/19hb5LE9YFe4G6Y3lFnmjh_DuAh_tzBIb/preview"
         className="md:w-3/5 md:h-[1080px] w-full h-[540px] mb-16"
       />
     </div>
